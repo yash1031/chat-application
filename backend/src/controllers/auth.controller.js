1b6f93acea994ae24c5bc494d763657c0ebd807a
yash1031 <yash735131@gmail.com>
2025-04-02 16:39:28 +0530

    Completing Login Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,8 +49,24 @@ export const signup= async (req, res)=>{
     }
 };
 
-export const login= (req, res)=>{
-    res.send("login route")
+export const login= async (req, res)=>{
+    try{
+        const user = await User.findOne({email});
+        
+        if(!user) {
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        })
+    } catch (error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
 
 export const logout= (req, res)=>{
