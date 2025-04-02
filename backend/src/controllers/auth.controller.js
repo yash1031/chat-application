e6ad0622a836420ad6e3d7ad1aa9e45417dbd0b2
yash1031 <yash735131@gmail.com>
2025-04-02 17:41:58 +0530

    Creating updateProfile Route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -85,6 +85,22 @@ export const logout= (req, res)=>{
     }
 };
 
-export const updateProfile= (req, res)=>{
-    
+export const updateProfile= async (req, res)=>{
+    try{
+        const {profilePic} = req.body;
+        const userID= req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message: "Profile pic is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userID, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in update profile", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+
+    }
 }
