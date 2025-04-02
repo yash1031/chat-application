588f845506edb7b4e8e601f5d956b90a9ddd8b50
yash1031 <yash735131@gmail.com>
2025-04-02 17:46:21 +0530

    Creating checkAuth Route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -104,3 +104,14 @@ export const updateProfile= async (req, res)=>{
 
     }
 }
+
+export const checkAuth = (req, res) =>{
+    try{
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+
+        
+    }
+}
