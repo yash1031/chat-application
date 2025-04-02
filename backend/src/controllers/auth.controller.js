327ac85096b23444afde795906c814f11839bd9e
yash1031 <yash735131@gmail.com>
2025-04-02 16:29:57 +0530

    Adding more checks into SignUp Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,13 +4,16 @@
 
 
 import { generateToken } from '../lib/utils.js';
+import bcrypt from "bcryptjs"
 import User from '../models/user.model.js'
 
 export const signup= async (req, res)=>{
     const {fullName, email, password}= req.body
     try{
-        //hash password
-        if(password.length< 6){
+        if (!fullName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+        if(password.length < 6){
             return res.status(400).json({message: "Password must be at least 6 characters"});
         }  
         const user = await User.findOne({email});
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB= async () =>{
     try {
-        const conn= await mongoose.connect(process.env.MONGODB_URI);
+        const conn= await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error){
         console.log("MongoDB connection error: ", error);
