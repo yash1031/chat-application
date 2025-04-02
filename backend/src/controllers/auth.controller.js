194fb414644d9faac321be9520daeeb1e1dc4938
yash1031 <yash735131@gmail.com>
2025-04-02 17:11:19 +0530

    Creating Logout Controller and some changes into Login Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -50,10 +50,16 @@ export const signup= async (req, res)=>{
 };
 
 export const login= async (req, res)=>{
+    
+    const {email, password}= req.body
     try{
         const user = await User.findOne({email});
         
         if(!user) {
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
             return res.status(400).json({message: "Invalid Credentials"});
         }
         generateToken(user._id, res);
@@ -70,5 +76,15 @@ export const login= async (req, res)=>{
 };
 
 export const logout= (req, res)=>{
-    res.send("logout route")
+    try{
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+    }catch (error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
+
+export const updateProfile= (req, res)=>{
+    
+}
