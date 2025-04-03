6f49b6f3d64ea3d6de40b1769f17db0d444cef89
yash1031 <yash735131@gmail.com>
2025-04-03 11:25:33 +0530

    Creating getUsersForSidebar and getMessages controllers

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,37 @@
-export const getUsersFromSidebar= async (req, res) =>{
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar= async (req, res) =>{
+    try{
+        const loggedInUserId= req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
+        res.status(200).json(filteredUsers);
+    } catch (error){
+        
+        console.log("Error in getUsersForSidebar", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+
+    }
+    
+}
+export const getMessages= async (req, res) =>{
+    try{
+        const {id: userToChatId}= req.params;
+        const myId= req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId},
+            ],
+        });
+
+        res.send(200).json(messages);
+    } catch (error){
+        
+        console.log("Error in getting messages", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+
+    }
     
 }
