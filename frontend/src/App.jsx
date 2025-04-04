59026607a6e6f9493ac6db966b6060914bcbb6f4
yash1031 <yash735131@gmail.com>
2025-04-04 22:17:26 +0530

    Setting up All Required Pages and Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+import SignUpPage from './pages/SignUpPage';
+import ProfilePage from './pages/ProfilePage';
 
 const App = () => {
   return (
-    <div>      
+    <div>
+      <Navbar/>      
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
