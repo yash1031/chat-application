fcc0cc03273415d8864d8a49d0fcf5d9d22f122e
yash1031 <yash735131@gmail.com>
2025-04-04 22:41:47 +0530

    Adding Cors logic to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import connectDB from "./lib/db.js";
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config()
 const app= express();
@@ -13,6 +14,10 @@ const PORT= process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
